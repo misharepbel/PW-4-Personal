361aa095a9718f9f4157f098808ca02a232a3699
Michael Tryfanau <michael.tryfanau@gmail.com>
2025-03-08 22:40:53 +0100

    Add project files.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace EShopService.Models
+{
+    public class Product : BaseModel
+    {
+        public string Ean { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string SKU { get; set; } = default!;
+        public Category Category { get; set; } = default!;
+        
+    }
+}
